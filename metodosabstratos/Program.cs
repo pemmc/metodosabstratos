5c19d4fddb6baf4a0997871e769b921b51817cb4
Paulo Eduardo Magaldi <pauloeduardomagaldi@hotmail.com>
2020-10-16 17:49:42 -0300

    dsd

---
+++ metodosabstratos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace metodosabstratos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
